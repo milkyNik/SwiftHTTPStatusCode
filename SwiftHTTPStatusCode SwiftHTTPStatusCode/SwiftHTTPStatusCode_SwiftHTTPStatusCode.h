c2d5ebc373a68ffe06a3393a7035767e20847de6
Nikita Milko <milniklaus@gmail.com>
2023-02-01 23:13:02 +0200

    Init project

---
+++ SwiftHTTPStatusCode SwiftHTTPStatusCode/SwiftHTTPStatusCode_SwiftHTTPStatusCode.h
@@ -0,0 +1,18 @@
+//
+//  SwiftHTTPStatusCode_SwiftHTTPStatusCode.h
+//  SwiftHTTPStatusCode SwiftHTTPStatusCode
+//
+//  Created by Nikita Milko on 01.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftHTTPStatusCode_SwiftHTTPStatusCode.
+FOUNDATION_EXPORT double SwiftHTTPStatusCode_SwiftHTTPStatusCodeVersionNumber;
+
+//! Project version string for SwiftHTTPStatusCode_SwiftHTTPStatusCode.
+FOUNDATION_EXPORT const unsigned char SwiftHTTPStatusCode_SwiftHTTPStatusCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHTTPStatusCode_SwiftHTTPStatusCode/PublicHeader.h>
+
+
